0362577fcc33e45113718e34e90008d773d084c6
David Konsumer <konsumer@jetboystudio.com>
2017-08-17 13:46:33 -0700

    better name

--- cli.js
+++ cli.js
@@ -4,7 +4,8 @@ var svgtojsx = require('svg-to-jsx')
 var readFile = require('fs').readFileSync
 var basename = require('path').basename
 var indentString = require('indent-string')
-var inflection = require('inflection')
+var classify = require('inflection').classify
+var parseString = new (require('xml2js')).Parser().parseString
 
 var args = require('yargs')
   .usage('Usage: $0 <SVG_FILE>')
@@ -16,19 +17,25 @@ var args = require('yargs')
   .demand(1, 'SVG_FILE is required.')
   .argv
 
-svgtojsx(readFile(args._[0])).then(function (jsx) {
-  var name = inflection.classify(basename(args._[0], '.svg'))
-  if (name.slice(0, 13) === 'InkExtXxxxxx.') {
-    name = inflection.classify(name.slice(13))
+var svg = readFile(args._[0]).toString()
+
+parseString(svg, function (err, result) {
+  if (err) {
+    console.error(err)
+    process.exit(1)
   }
-  console.log([
-    'export const ' + name + ' = () => (',
-    indentString(jsx, 2),
-    ')',
-    'export default ' + name
-  ].join('\n'))
-})
-.catch(function (err) {
-  console.error(err)
-  process.exit(1)
+  var name = classify(basename(svg['$']['sodipodi:docname'], '.jsx'))
+  svgtojsx(svg)
+    .then(function (jsx) {
+      console.log([
+        'export const ' + name + ' = () => (',
+        indentString(jsx, 2),
+        ')',
+        'export default ' + name
+      ].join('\n'))
+    })
+    .catch(function (err) {
+      console.error(err)
+      process.exit(1)
+    })
 })
